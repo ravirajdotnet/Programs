d9efa0e90b503c3130e72c2e8d3d460fdd28dad6
ravirajdotnet <kraviraj4474@gmail.com>
2025-01-22 10:05:47 +0530

    Add files via upload

---
+++ Method/Method/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    internal class Program
+    {
+
+        public void NoReturn() 
+        {
+            Console.WriteLine("If we use void No retun type");
+        }
+        public static int Add(int a, int b) 
+        {
+            int result = a + b;
+            return result;
+        }
+
+        public int sub(int a, int b) 
+        {
+            int result = a - b;
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.NoReturn();
+            int subres = obj.sub(4, 2);
+            Console.WriteLine(subres);
+
+            // Using Static, method syntax 
+            int res = Program.Add(6,7);
+            Console.WriteLine(res);
+
+            Console.ReadLine();
+        }
+    }
+}
