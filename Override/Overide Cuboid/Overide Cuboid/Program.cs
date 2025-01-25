3970f68901b180916175ca1fd3a50e201b4c9901
ravirajdotnet <kraviraj4474@gmail.com>
2025-01-25 18:36:03 +0530

    Add files via upload

---
+++ Override/Overide Cuboid/Overide Cuboid/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Inheritance_in_Class
+{
+    class Rectangle
+    {
+        int Length, Breadth, Perimeter;
+        protected int Area;
+        public virtual void AcceptData()
+        {
+            Console.WriteLine("Enter the Length of a Rectangle = ");
+            this.Length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Breadth of a Rectangle = ");
+            this.Breadth = Convert.ToInt32(Console.ReadLine());
+        }
+        public virtual void Calculate()
+        {
+            this.Area = this.Length * this.Breadth;
+            this.Perimeter = 2 * (this.Length + this.Breadth);
+        }
+        public override string ToString()
+        {
+            string Output = "The Area of the Rectangle = " + this.Area + "\nThe Perimeter of the Rectangle = " +
+            this.Perimeter;
+            return Output;
+        }
+        class Cuboid : Rectangle
+        {
+            int Height, volume;
+            public override void AcceptData()
+            {
+                base.AcceptData();
+                Console.WriteLine("Enter the Height of the Cuboid = ");
+                this.Height = Convert.ToInt32(Console.ReadLine());
+            }
+            public override void Calculate()
+            {
+                base.Calculate();
+                this.volume = base.Area * this.Height;
+            }
+            public override string ToString()
+            {
+                string Output = "The volume of the Cuboid is = " + this.volume;
+                return Output;
+            }
+        }
+
+        class Example_04
+        {
+            static void Main()
+            {
+                Cuboid C1 = new Cuboid();
+                C1.AcceptData();
+                C1.Calculate();
+
+                Console.WriteLine(C1);
+                Console.ReadKey();
+
+                //Rectangle R1 = new Rectangle();
+                //R1.AcceptData();
+                //R1.Calculate();
+
+                //Console.WriteLine(R1);
+                //Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Override/Override Cube/Override Cube/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Override_Cube
+{
+    class Square
+    {
+        int Length, Perimeter;
+        protected int Area;
+        public virtual void AccetpData()
+        {
+            Console.WriteLine("Enter the Length of the Square");
+            this.Length = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public virtual void Calculate()
+        {
+            this.Area = this.Length * this.Length;
+            this.Perimeter = 4 * this.Area;
+        }
+
+        public override string ToString()
+        {
+            string Output = "Area of Square = " + this.Area;
+            return Output;
+
+        }
+    }
+    class Cube : Square
+    {
+        int Volume;
+        public override void AccetpData()
+        {
+            base.AccetpData();
+            Console.WriteLine("Enter the Height of the Square");
+            this.Volume = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public override void Calculate()
+        {
+            base.Calculate();
+            this.Volume = base.Area * 4;
+        }
+
+        public override string ToString()
+        {
+            string Output = "Volume of the Cube = " + this.Volume;
+            return Output;
+
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Square S1 = new Square();
+                S1.AccetpData();
+                S1.Calculate();
+                Console.WriteLine(S1);
+
+                Cube C1 = new Cube();
+                C1.AccetpData();
+                C1.Calculate();
+                Console.WriteLine(C1);
+
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+}
